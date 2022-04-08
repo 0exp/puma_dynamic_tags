5d0c7283190498603bc00c86876baa64b7f66db7
Rustam Ibragimov <iamdaiver@gmail.com>
2022-04-08 03:10:16 +0300

    require version too

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.2.0] - 2022-04-08
+
+- Initial release
+
 ## [0.1.2] - 2022-04-08
 
 - Initial release
--- lib/puma/plugin/dynamic_tags.rb
+++ lib/puma/plugin/dynamic_tags.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
-Puma::Plugin.create { require_relative 'dynamic_tags/puma_patches' }
+Puma::Plugin.create do
+  require_relative 'dynamic_tags/puma_patches'
+  require_relative 'dynamic_tags/version'
+end
